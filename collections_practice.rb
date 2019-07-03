ad65542bae510a8faef39f94e90c052f0b4c7980
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-07-03 18:37:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,7 +38,7 @@ def kesha_maker(array)
   array.each do |letter|
     new_letter = letter.split""
     new_letter[2] = "$"
-    kesha << new_letter
+    kesha << new_letter.join
   end
   kesha
 end
